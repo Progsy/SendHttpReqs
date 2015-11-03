00cab20a317975a464b1a99d7d9c979d19078f15
Progsy <halldinoscar@gmail.com>
2015-11-03 17:23:05 +0100

    Added basic function

--- SendHttpReqs/Form1.Designer.cs
+++ SendHttpReqs/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.reqpayloadBox = new System.Windows.Forms.RichTextBox();
+            this.sendReqButton = new System.Windows.Forms.Button();
+            this.responseBox = new System.Windows.Forms.RichTextBox();
+            this.serverrespBox = new System.Windows.Forms.RichTextBox();
+            this.contenttypeBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // urlBox
+            // 
+            this.urlBox.Location = new System.Drawing.Point(13, 13);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(580, 20);
+            this.urlBox.TabIndex = 0;
+            this.urlBox.Text = resources.GetString("urlBox.Text");
+            // 
+            // reqpayloadBox
+            // 
+            this.reqpayloadBox.Location = new System.Drawing.Point(13, 185);
+            this.reqpayloadBox.Name = "reqpayloadBox";
+            this.reqpayloadBox.Size = new System.Drawing.Size(241, 155);
+            this.reqpayloadBox.TabIndex = 1;
+            this.reqpayloadBox.Text = resources.GetString("reqpayloadBox.Text");
+            // 
+            // sendReqButton
+            // 
+            this.sendReqButton.Location = new System.Drawing.Point(518, 317);
+            this.sendReqButton.Name = "sendReqButton";
+            this.sendReqButton.Size = new System.Drawing.Size(75, 23);
+            this.sendReqButton.TabIndex = 3;
+            this.sendReqButton.Text = "Send";
+            this.sendReqButton.UseVisualStyleBackColor = true;
+            this.sendReqButton.Click += new System.EventHandler(this.sendReqButton_Click);
+            // 
+            // responseBox
+            // 
+            this.responseBox.Location = new System.Drawing.Point(261, 40);
+            this.responseBox.Name = "responseBox";
+            this.responseBox.Size = new System.Drawing.Size(332, 139);
+            this.responseBox.TabIndex = 4;
+            this.responseBox.Text = "Response";
+            // 
+            // serverrespBox
+            // 
+            this.serverrespBox.Location = new System.Drawing.Point(261, 186);
+            this.serverrespBox.Name = "serverrespBox";
+            this.serverrespBox.Size = new System.Drawing.Size(332, 125);
+            this.serverrespBox.TabIndex = 5;
+            this.serverrespBox.Text = "Response From Server";
+            // 
+            // contenttypeBox
+            // 
+            this.contenttypeBox.Location = new System.Drawing.Point(13, 40);
+            this.contenttypeBox.Name = "contenttypeBox";
+            this.contenttypeBox.Size = new System.Drawing.Size(241, 20);
+            this.contenttypeBox.TabIndex = 6;
+            this.contenttypeBox.Text = "application/x-www-form-urlencoded;charset=UTF-8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(605, 352);
+            this.Controls.Add(this.contenttypeBox);
+            this.Controls.Add(this.serverrespBox);
+            this.Controls.Add(this.responseBox);
+            this.Controls.Add(this.sendReqButton);
+            this.Controls.Add(this.reqpayloadBox);
+            this.Controls.Add(this.urlBox);
+            this.Name = "Form1";
+            this.Text = "HttpRequest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox urlBox;
+        private System.Windows.Forms.RichTextBox reqpayloadBox;
+        private System.Windows.Forms.Button sendReqButton;
+        private System.Windows.Forms.RichTextBox responseBox;
+        private System.Windows.Forms.RichTextBox serverrespBox;
+        private System.Windows.Forms.TextBox contenttypeBox;
     }
 }
 
--- SendHttpReqs/Form1.cs
+++ SendHttpReqs/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,41 @@ namespace SendHttpReqs
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void sendReqButton_Click(object sender, EventArgs e)
+        {
+            if (urlBox.Text != null && urlBox.Text != "") 
+            {
+                CreateRequest();
+            }
+        }
+
+        void CreateRequest() 
+        {
+            string postData = reqpayloadBox.Text;
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            WebRequest request = WebRequest.Create(urlBox.Text);
+            request.UseDefaultCredentials = true;
+            request.Method = "POST";
+            request.ContentLength = byteArray.Length;
+            request.ContentType = contenttypeBox.Text;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            WebResponse response = request.GetResponse();
+            responseBox.Text = ((HttpWebResponse)response).StatusDescription;
+
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+            serverrespBox.Text = responseFromServer;
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
         }
     }
 }
